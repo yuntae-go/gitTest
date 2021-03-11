c1038468c9c87cc264678bafe5188645d19c8539
SeunghanJI <SeunghanJI@users.noreply.github.com>
2021-03-11 22:17:26 +0900

    test users

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 const test = 22;
+
+const users = [
+    { name: "고윤태", age: 23, gender: 1, address: "서울특별시 중랑구" },
+    { name: "황대명", age: 23, gender: 1, address: "서울특별시 도봉구" },
+    { name: "지승한", age: 25, gender: 1, address: "서울특별시 동대문구" },
+    { name: "신영석", age: 23, gender: 1, address: "경기도 고양시" }
+];
