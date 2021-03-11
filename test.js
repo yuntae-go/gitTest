808bb5532fb5bae9dfb33e46a00df57a8d1178e6
sinyeongseok <sinyeongseok99@gmail>
2021-03-11 22:41:16 +0900

    z

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 const test = 22;
-<<<<<<< HEAD
 
 const users = [
     { name: "고윤태", age: 23, gender: 1, address: "서울특별시 중랑구" },
@@ -7,7 +6,7 @@ const users = [
     { name: "지승한", age: 25, gender: 1, address: "서울특별시 동대문구" },
     { name: "신영석", age: 23, gender: 1, address: "경기도 고양시" }
 ];
-=======
+
 const a = () =>{
     test = test +1;
 }
@@ -15,4 +14,3 @@ const a = () =>{
 const b = () =>{
     test = test -1;
 }
->>>>>>> 347734307f4b1ce9d5385814bc387d9fc3d0056f
